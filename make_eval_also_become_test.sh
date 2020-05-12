a35b8944b611db58588cf501f7fbe1be9fa1b664
PeterZhizhin <piter.zh@gmail.com>
2020-05-12 18:10:04 +0300

    Fixed symlinks for eval.

--- make_eval_also_become_test.sh
+++ make_eval_also_become_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-ln -s uncertainty_dataset/output_datasets/result/wiki/dev.txt \
+ln -s dev.txt \
       uncertainty_dataset/output_datasets/result/wiki/test.txt
 
-ln -s uncertainty_dataset/output_datasets/result/bio/dev.txt \
+ln -s dev.txt \
       uncertainty_dataset/output_datasets/result/bio/test.txt
 
-ln -s uncertainty_dataset/output_datasets/result/classification/wiki/dev.txt \
+ln -s dev.txt \
       uncertainty_dataset/output_datasets/result/classification/wiki/test.txt
 
-ln -s uncertainty_dataset/output_datasets/result/classification/bio/dev.txt \
+ln -s dev.txt \
       uncertainty_dataset/output_datasets/result/classification/bio/test.txt
