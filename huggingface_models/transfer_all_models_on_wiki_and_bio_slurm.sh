c3937d9cc22ac9c947cf89d3bea1cd9249e76b4a
PeterZhizhin <piter.zh@gmail.com>
2020-05-16 22:39:18 +0300

    Added Electra training.

--- huggingface_models/transfer_all_models_on_wiki_and_bio_slurm.sh
+++ huggingface_models/transfer_all_models_on_wiki_and_bio_slurm.sh
@@ -2,17 +2,18 @@
 #SBATCH -n 1 -c 1 -G 1
 #SBATCH --error=ner_experiments.%A_%a.error
 #SBATCH --output=ner_experiments.%A_%a.out
-#SBATCH --array=0-89
+#SBATCH --array=0-39
 
 BASE_DATA_DIR=../uncertainty_dataset/output_datasets/result
+BASE_OUTPUT_DIR="ner_experiments"
 
-# 30 runs in total 3 * 2 * 15 = 90
-declare -a model_paths=("bert-base-cased" "allenai/scibert_scivocab_cased" "biobert_base")
-declare -a output_base_paths=("bert_base_cased" "scibert_scivocab_cased" "biobert_base")
+# 30 runs in total 1 * 2 * 20 = 40 (training only electra)
+declare -a model_paths=("google/electra-small-discriminator" "bert-base-cased" "allenai/scibert_scivocab_cased" "biobert_base")
+declare -a output_base_paths=("electra_small_discriminator" "bert_base_cased" "scibert_scivocab_cased" "biobert_base")
 declare -a dataset_paths=("wiki" "bio")
 declare -a dataset_transfers=("bio" "wiki")
-seeds_start=6
-number_of_seeds=15
+seeds_start=1
+number_of_seeds=20
 
 let seeds_end=$seeds_start+$number_of_seeds-1
 echo $seeds_start $seeds_end
@@ -28,8 +29,8 @@ do
     dataset_transfer=${dataset_transfers[$j]}
     for seed in $(seq $seeds_start $seeds_end)
     do
-      current_model_path="${current_output_base_path}_${dataset_part}_${seed}"
-      full_output_dir="${current_output_base_path}_${dataset_part}_to_${dataset_transfer}_${seed}"
+      current_model_path="$BASE_OUTPUT_DIR/${current_output_base_path}_${dataset_part}_${seed}"
+      full_output_dir="$BASE_OUTPUT_DIR/${current_output_base_path}_${dataset_part}_to_${dataset_transfer}_${seed}"
       full_data_dir="$BASE_DATA_DIR/$dataset_transfer"
       full_argument_list="$current_model_path $full_output_dir $full_data_dir $seed"
       all_arguments_list+=("$full_argument_list")
